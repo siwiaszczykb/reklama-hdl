c33d90acc05452cc6fe83e3c85fa1c6460435df1
Bartosz Siwiaszczyk <bs305221@student.polsl.pl>
2024-12-27 15:33:26 +0100

    Commit plikow wygenerowanych przez Intel Quartusa

---
+++ reklama.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity reklama is
+port (clk: in std_logic;
+		display0: in std_logic_vector(5 downto 0);
+		display1: in std_logic_vector(5 downto 0);
+		display2: in std_logic_vector(5 downto 0);
+		display3: in std_logic_vector(5 downto 0);
+		display4: in std_logic_vector(5 downto 0);
+		display5: in std_logic_vector(5 downto 0) );
+end reklama;
+
+architecture scroll of reklama is 
+
+end scroll;
