3986025dbb743f79e1a661b9237cd4a1aae53c14
Bartosz Siwiaszczyk <bs305221@student.polsl.pl>
2025-01-04 23:12:21 +0100

    poprawki rejestru, dzialajacy glowny uklad

--- rejestr.vhd
+++ rejestr.vhd
@@ -5,22 +5,25 @@ use IEEE.std_logic_unsigned.all;
 entity rejestr is
     port (
         clk : in std_logic;
+		  EN : in std_logic;
 		  D : in std_logic_vector(6 downto 0);
         Q : out std_logic_vector(6 downto 0));
 end rejestr;
 
 architecture przesuwanie of rejestr is 
 begin
-    process(clk)
+    process(EN, clk)
     begin
+	 if EN = '1' then
         if clk'event and clk = '1' then
-		  		Q(6) <= not D(6);
-				Q(5) <= not D(5);
-				Q(4) <= not D(4);
-				Q(3) <= not D(3);
-				Q(2) <= not D(2);
-				Q(1) <= not D(1);
-				Q(0) <= not D(0);
+		  		Q(6) <= D(6);
+				Q(5) <= D(5);
+				Q(4) <= D(4);
+				Q(3) <= D(3);
+				Q(2) <= D(2);
+				Q(1) <= D(1);
+				Q(0) <= D(0);
         end if;
+	 end if;
     end process;
 end przesuwanie;
